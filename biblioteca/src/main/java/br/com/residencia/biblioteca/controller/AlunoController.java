01525a711828fd051ce031dbe1681b5024c60381
wellington-santos <wellingtonsantosceo@gmail.com>
2022-10-07 11:43:11 -0300

    aluno update

--- biblioteca/src/main/java/br/com/residencia/biblioteca/controller/AlunoController.java
+++ biblioteca/src/main/java/br/com/residencia/biblioteca/controller/AlunoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,12 +28,12 @@ public class AlunoController {
 	@Autowired
 	AlunoService alunoService;
 
-	@GetMapping
+	@GetMapping("/search")
 	public ResponseEntity<List<Aluno>> getAllAlunos() {
 		return new ResponseEntity<>(alunoService.getAllAlunos(), HttpStatus.OK);
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping("/search/{id}")
 	public ResponseEntity<Aluno> getAlunoById(@PathVariable Integer id) {
 		return new ResponseEntity<>(alunoService.getAlunoById(id), HttpStatus.OK);
 	}
@@ -45,5 +46,10 @@ public class AlunoController {
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<Aluno> deleteAluno(@PathVariable Integer id) {
 		return new ResponseEntity<>(alunoService.deleteAluno(id), HttpStatus.OK);
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Aluno> updateAluno(@RequestBody Aluno aluno, @PathVariable Integer id) {
+		return new ResponseEntity<>(alunoService.updateAluno(aluno, id), HttpStatus.OK);
 	}
 }
