4821e61edd42544719295d1ceb0e80a8e2f4ddb0
wellington-santos <wellingtonsantosceo@gmail.com>
2022-10-13 21:42:29 -0300

    att dto

--- biblioteca/src/main/java/br/com/residencia/biblioteca/BibliotecaApplication.java
+++ biblioteca/src/main/java/br/com/residencia/biblioteca/BibliotecaApplication.java
@@ -11,6 +11,7 @@ public class BibliotecaApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
 	}
